ecd381276b009c29eb50b0f292381d507379b75c
Vlad Chubar <aromudez@gmail.com>
2022-03-29 21:02:33 +0300

    push

--- lesson13/tasks/task2/calculator.test.js
+++ lesson13/tasks/task2/calculator.test.js
@@ -1,7 +1,7 @@
 import { it } from 'eslint/lib/rule-tester/rule-tester';
 import getSum, { getSquaredNumbers, getOddNumbers } from './calculator';
 it('should get squared numbers', () => {
-  const result = getSquaredNumbers([1, 2, 3]);
+  const result = getSquaredArray([1, 2, 3]);
   expect(result).toEqual([1, 4, 9]);
 });
 it('should keep odd numbers only', () => {
