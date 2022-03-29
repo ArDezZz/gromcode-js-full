345d22d5708cf1e387348a7deb7fa1b0d171de56
Vlad Chubar <aromudez@gmail.com>
2022-03-29 21:51:52 +0300

    fix

--- lesson13/tasks/task3/index.js
+++ lesson13/tasks/task3/index.js
@@ -3,7 +3,7 @@ const getMinSquaredNumber = arr => {
     return null;
   }
   const res = arr.map(el => Math.abs(el));
-  const res1 = Math.min.apply(null, res);
+  const res1 = Math.min(...res);
   return res1 * res1;
 };
 
