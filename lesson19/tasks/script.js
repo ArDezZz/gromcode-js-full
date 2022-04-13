ab5afd27e97dccb51bb62e54eef46ddf0cb4a448
ArDezZz <aromudez@gmail.com>
2022-04-13 20:29:23 +0300

    fix1

--- lesson19/tasks/script.js
+++ lesson19/tasks/script.js
@@ -6,90 +6,78 @@
 // const { exceedsLimit } = require('prettier');
 'use strict';
 //=====================task1===================
-// export const makeCounter = () => {
-//   let count = 0;
-//   return function () {
-//     return count++;
-//   }
-// };
-// const counter1 = makeCounter();
-// const counter2 = makeCounter();
-// console.log(counter1());
-// console.log(counter1());
-// console.log(counter2());
-// console.log(counter1());
-// console.log(counter2());
-//======================task2===================
-// input: number
-// output: object
-// export function createCalculator() {
-//   let number = 0;
-//   function add(a) {
-//     number += a;
-//   }
-//   function decrease(b) {
-//     number -= b;
-//   }
-//   function reset() {
-//     number = 0;
-//   }
-//   function getMemo() {
-//     return number;
-//   }
-//   return {
-//     add,
-//     decrease,
-//     reset,
-//     getMemo,
-//   };
-// }
-// const calc = createCalculator();
-// console.log(calc());
-//=========================task3==================
-'use strict';
-
-/**
- * @return {object}
- */
-export const createLogger = () => {
-  let memory = [];
-  let warn = text => memory.unshift({ message: text, dateTime: new Date(), type: 'warn' });
-  let error = text => memory.unshift({ message: text, dateTime: new Date(), type: 'error' });
-  let log = text => memory.unshift({ message: text, dateTime: new Date(), type: 'log' });
-  let getRecords = text => {
-    if (text == 'warn') return memory.filter(obj => obj.type == 'warn');
-    if (text == 'error') return memory.filter(obj => obj.type == 'error');
-    if (text == 'log') return memory.filter(obj => obj.type == 'log');
-    return memory;
-  };
-  let logout = {
-    warn,
-    error,
-    log,
-    getRecords,
-    memory,
-  };
-  return logout;
+const vehicle = {
+  name: 'Argo',
+  move() {
+    console.log(`${this.name} is moving`);
+  },
+  stop() {
+    console.log(`${this.name} stopped`);
+  },
 };
-
-// examples
-const logger1 = createLogger();
-logger1.log('User logged in');
-logger1.warn('User is tring to ented restricted page');
-logger1.log('User logged out');
-logger1.error('Unexpected error on the site');
-
-console.log(logger1.getRecords()); // ===> [{ message: 'Unexpected error on the site', type: 'error', dateTime: Date }, { message: 'User logged out', type: 'log', dateTime: Date }, { message: 'User is tring to ented restricted page', type: 'warn', dateTime: Date }, { message: 'User logged in', type: 'log', dateTime: Date }]
-logger1.getRecords('log'); // ===> [{ message: 'User logged out', type: 'log', dateTime: Date }, { message: 'User logged in', type: 'log', dateTime: Date }]
-logger1.getRecords('error'); // ===> [{ message: 'Unexpected error on the site', type: 'error', dateTime: Date }]
-logger1.getRecords('warn'); // ===> [{ message: 'User is tring to ented restricted page', type: 'warn', dateTime: Date }]
-
-const logger2 = createLogger();
-logger2.warn('Opps, something is happenning');
-logger2.getRecords('error'); // ===> []
-logger2.getRecords('warn'); // ===> [{ message: 'Opps, something is happenning', type: 'warn', dateTime: Date }]
-logger2.getRecords(); // ===> [{ message: 'Opps, something is happenning', type: 'warn', dateTime: Date }]
-
-const logger3 = createLogger();
-logger3.getRecords('error'); // ===> []
-logger3.getRecords(); // ===> []
+const ship = {
+  __proto__: vehicle,
+  startMachine() {
+    console.log(`${this.name} lifting anchor up`);
+    this.move();
+  },
+  stopMachine() {
+    console.log(`${this.name} lifting anchor down`);
+    this.stop();
+  },
+};
+ship.startMachine();
+ship.stopMachine();
+//=======================task2=====================
+const obj1 = {
+  name: 'John',
+  age: '18',
+};
+function getOwnProps(ship) {
+  let resArrProp = [];
+  for (let key in ship) {
+    if (ship.hasOwnProperty(key) && typeof ship[key] !== 'function') {
+      resArrProp.push(key);
+    }
+  }
+  return resArrProp;
+}
+console.log(getOwnProps(obj1));
+//=====================task3======================
+function User(name, age) {
+  this.name = name;
+  this.age = age;
+}
+User.prototype.sayHi = function () {
+  console.log(`Hi, I am ${this.name}`);
+};
+User.prototype.requestNewPhoto = function () {
+  console.log(`New photo request was sent for ${this.name}`);
+};
+User.prototype.setAge = function (age) {
+  this.age = age;
+  if (age < 0) {
+    return false;
+  }
+  if (age >= 25) {
+    console.log(`New photo request was sent for ${this.name}`);
+  }
+  return age;
+};
+const user1 = new User('Tom', -1);
+const user2 = new User('Bob', 27);
+user1.setAge();
+//=====================task4======================
+const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  set fullName(value) {
+    const [firstName, lastName] = value.split(' ');
+    this.firstName = firstName;
+    this.lastName = lastName;
+  },
+};
+export default user;
