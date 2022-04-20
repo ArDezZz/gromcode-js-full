f32a2080be0cb63395d21fd489c37c7cbb4b95ae
ArDezZz <aromudez@gmail.com>
2022-04-20 20:37:05 +0300

    fix2

--- lesson21/task2/index.js
+++ lesson21/task2/index.js
@@ -1,9 +1,9 @@
-const getTitleElement = () => {
+export const getTitleElement = () => {
   const titleElem = document.querySelector('.title');
   console.dir(titleElem);
   return titleElem;
 };
-const getInputElement = () => {
+export const getInputElement = () => {
   const inputElem = document.querySelector('text');
   console.dir(inputElem);
   return inputElem;
