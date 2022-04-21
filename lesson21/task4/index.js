9b3cb3a39f2c8fff44e73bfa13692d2fc5da9a0a
ArDezZz <aromudez@gmail.com>
2022-04-21 14:19:06 +0300

    delete console.log

--- lesson21/task4/index.js
+++ lesson21/task4/index.js
@@ -2,22 +2,18 @@ export function getTitle() {
   const elemTitle = document.querySelector('.title');
   return elemTitle.textContent;
 }
-console.log(getTitle());
 
 export function getDescription() {
   const elemAbout = document.querySelector('.about');
   return elemAbout.innerText;
 }
-console.log(getDescription());
 
 export function getPlans() {
   const elemPlans = document.querySelector('.plans');
   return elemPlans.innerHTML;
 }
-console.log(getPlans());
 
 export function getGoal() {
   const elemGoal = document.querySelector('.goal');
   return elemGoal.outerHTML;
 }
-console.log(getGoal());
