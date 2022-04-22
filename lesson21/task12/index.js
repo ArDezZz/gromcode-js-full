77a2d39c74996b556e6609b9054868fe24406e6a
ArDezZz <aromudez@gmail.com>
2022-04-22 21:27:15 +0300

    fixtask12

--- lesson21/task12/index.js
+++ lesson21/task12/index.js
@@ -1,14 +1,22 @@
-export function squaredNumbers() {
-  let itemList1 = document.querySelector('.number');
-  let dataNumber1 = itemList1.dataset.number;
-  itemList1.setAttribute('data-squared-number', dataNumber1 * dataNumber1);
+// export function squaredNumbers() {
+//   let itemList1 = document.querySelector('.number');
+//   let dataNumber1 = itemList1.dataset.number;
+//   itemList1.setAttribute('data-squared-number', dataNumber1 * dataNumber1);
 
-  let itemList2 = itemList1.nextElementSibling;
-  let dataNumber2 = itemList2.dataset.number;
-  itemList2.setAttribute('data-squared-number', dataNumber2 * dataNumber2);
+//   let itemList2 = itemList1.nextElementSibling;
+//   let dataNumber2 = itemList2.dataset.number;
+//   itemList2.setAttribute('data-squared-number', dataNumber2 * dataNumber2);
 
-  let itemList3 = itemList2.nextElementSibling;
-  let dataNumber3 = itemList3.dataset.number;
-  itemList3.setAttribute('data-squared-number', dataNumber3 * dataNumber3);
+//   let itemList3 = itemList2.nextElementSibling;
+//   let dataNumber3 = itemList3.dataset.number;
+//   itemList3.setAttribute('data-squared-number', dataNumber3 * dataNumber3);
+// }
+// squaredNumbers();
+function squaredNumbers() {
+  const numbersElements = document.querySelectorAll('.number');
+  [...numbersElements].map(elem => {
+    elem.dataset.squaredNumber = Math.pow(elem.dataset.number, 2);
+  });
+  return numbersElements;
 }
-squaredNumbers();
+console.log(squaredNumbers());
