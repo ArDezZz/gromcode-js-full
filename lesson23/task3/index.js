19070d1359287ef41b412f0ea6f7ac140d419296
ArDezZz <aromudez@gmail.com>
2022-05-02 11:50:13 +0300

    lesson23-24

--- lesson23/task3/index.js
+++ lesson23/task3/index.js
@@ -1,13 +1,66 @@
-const getItemsList = () => {
-  const elementsList = document.querySelectorAll('.technology');
-  console.dir(elementsList);
-  return elementsList;
+const tasks = [
+  { text: 'Buy milk', done: false, id: 1 },
+  { text: 'Pick up Tom from airport', done: false, id: 2 },
+  { text: 'Visit party', done: false, id: 3 },
+  { text: 'Visit doctor', done: true, id: 4 },
+  { text: 'Buy meat', done: true, id: 5 },
+];
+
+const listElem = document.querySelector('.list');
+const checkbox = document.querySelector('.list__item-checkbox');
+const buttonELem = document.querySelector('.create-task-btn');
+
+const renderTasks = tasksList => {
+  listElem.innerHTML = '';
+
+  const tasksElems = tasksList
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done, id }) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+      const checkbox = document.createElement('input');
+      checkbox.setAttribute('type', 'checkbox');
+      checkbox.checked = done;
+      checkbox.dataset.id = id;
+      checkbox.classList.add('list__item-checkbox');
+      listItemElem.append(checkbox, text);
+
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+
+      return listItemElem;
+    });
+
+  listElem.append(...tasksElems);
 };
-getItemsList();
-const getItemsArray = () => {
-  const elementsArray = document.querySelectorAll('.tool');
-  const finalArray = Array.from(elementsArray);
-  console.dir(finalArray);
-  return finalArray;
+
+renderTasks(tasks);
+
+const changeTaskStatus = event => {
+  const taskId = Number(event.target.dataset.id);
+  const currentTask = tasks.find(task => task.id === taskId);
+  currentTask.done = !currentTask.done;
+  renderTasks(tasks);
 };
-getItemsArray();
+
+listElem.addEventListener('click', changeTaskStatus);
+
+const addTasks = () => {
+  const inputElem = document.querySelector('.task-input');
+  const object = {};
+  const newInput = inputElem.value;
+
+  if (!newInput) {
+    return;
+  }
+
+  object.text = newInput;
+  object.done = false;
+  object.id = tasks.length + 1;
+  tasks.unshift(object);
+  inputElem.value = '';
+  renderTasks(tasks);
+};
+
+buttonELem.addEventListener('click', addTasks);
