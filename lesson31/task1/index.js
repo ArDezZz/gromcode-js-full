7791776f5f7b4c75e5ab9a8d1170138c0cb20207
ArDezZz <aromudez@gmail.com>
2022-05-11 14:08:40 +0300

    lesson31

---
+++ lesson31/task1/index.js
@@ -0,0 +1,27 @@
+const requestUserData = userId => {
+  const request = new Promise((resolve, reject) => {
+    if (userId === 'broken') {
+      setTimeout(() => {
+        reject(new Error('User not found'));
+      }, 500);
+    } else {
+      setTimeout(() => {
+        resolve({
+          name: 'John',
+          age: 17,
+          userId,
+          email: `${userId}@example.com`,
+        });
+      }, 1000);
+    }
+  });
+  return request;
+};
+requestUserData('broken')
+  .catch(error => {
+    console.log(error);
+    throw new Error('Error');
+  })
+  .then(data => console.log(data))
+  .catch(err => console.log(err))
+  .finally(() => console.log('finally'));
