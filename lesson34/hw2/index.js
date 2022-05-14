121414addd8364aa6155b5f701e1b63f85003f17
ArDezZz <aromudez@gmail.com>
2022-05-14 16:19:25 +0300

    fix1

--- lesson34/hw2/index.js
+++ lesson34/hw2/index.js
@@ -3,10 +3,11 @@ const baseUrl = 'https://627ea7fc271f386ceffbc3ba.mockapi.io/api/v1/users/';
 let btnRegister = document.querySelector('.submit-button');
 
 const sendDataHandler = () => {
-  let elemsInputEmail = document.querySelector('input[type=email]');
-  let elemsInputName = document.querySelector('input[type=text]');
-  let elemsInputPass = document.querySelector('input[type=password]');
-  console.log([elemsInputEmail.value, elemsInputName.value, elemsInputPass.value]);
+  let elemsInputEmail = document.querySelector('input[type=email]').value;
+  let elemsInputName = document.querySelector('input[type=text]').value;
+  let elemsInputPass = document.querySelector('input[type=password]').value;
+  let res = [elemsInputEmail, elemsInputName, elemsInputPass];
+  console.log(res);
 };
 
 btnRegister.addEventListener('click', sendDataHandler);
