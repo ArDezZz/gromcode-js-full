e865469f94f053b76ffd5e6e83c357f69e01329f
ArDezZz <aromudez@gmail.com>
2022-05-15 11:46:31 +0300

    fix5

--- lesson34/hw1/index.js
+++ lesson34/hw1/index.js
@@ -1,11 +1,11 @@
-const baseUrl = 'https://627ea7fc271f386ceffbc3ba.mockapi.io/api/v1/users/';
+const baseUrl = 'https://627ea7fc271f386ceffbc3ba.mockapi.io/api/v1/users';
 
 export function getUsersList() {
   return fetch(baseUrl).then(response => response.json());
 }
 
 export function getUserById(userId) {
-  return fetch(`${baseUrl}${userId}`).then(response => response.json());
+  return fetch(`${baseUrl}/${userId}`).then(response => response.json());
 }
 
 export function createUser(userData) {
@@ -19,13 +19,13 @@ export function createUser(userData) {
 }
 
 export function deleteUser(userId) {
-  return fetch(`${baseUrl}${userId}`, {
+  return fetch(`${baseUrl}/${userId}`, {
     method: 'DELETE',
   });
 }
 
 export function updateUser(userId, userData) {
-  return fetch(`${baseUrl}${userId}`, {
+  return fetch(`${baseUrl}/${userId}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json;charset=utf-8',
